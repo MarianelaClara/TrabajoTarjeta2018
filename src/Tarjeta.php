1f9fb94efd64e5117131320f8dc448719a95ee00
MarianelaClara <marianelaclara@gmail.com>
2018-08-09 20:30:47 -0300

    Corrijo recarga

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -3,15 +3,25 @@
 namespace TrabajoTarjeta;
 
 class Tarjeta implements TarjetaInterface {
-    protected $saldo;
+    public $saldo;
 
     public function recargar($monto) {
-      // Esto esta hecho mal a proposito.
-      if ($monto % 2 == 0) {
+
+      if ($monto == 10 || $monto == 20 || $monto == 30 || $monto == 50 || $monto == 100) {
         $this->saldo += $monto;
+	return TRUE;
       }
 
-      return $monto % 2 == 0;
+      if ($monto == 510.15) {
+	$this->saldo += ($monto + 81.93);
+	return TRUE;
+      }
+      if ($monto == 962.59) {
+	$this->saldo += ($monto + 221.58);
+	return TRUE;
+      }
+
+      return FALSE;
     }
 
     /**
