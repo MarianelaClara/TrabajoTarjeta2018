c756b7f66d69af64d647fdc495b8a1d190a6d1c6
MarianelaClara <marianelaclara@gmail.com>
2018-08-17 16:59:29 -0300

    correccion

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -17,6 +17,23 @@ class TarjetaTest extends TestCase {
 
         $this->assertTrue($tarjeta->recargar(20));
         $this->assertEquals($tarjeta->obtenerSaldo(), 30);
+
+        $this->assertTrue($tarjeta->recargar(30));
+        $this->assertEquals($tarjeta->obtenerSaldo(), 60);
+
+        $this->assertTrue($tarjeta->recargar(50));
+        $this->assertEquals($tarjeta->obtenerSaldo(), 110);
+
+        $this->assertTrue($tarjeta->recargar(100));
+        $this->assertEquals($tarjeta->obtenerSaldo(), 210);
+
+        $this->assertTrue($tarjeta->recargar(510,15));
+        $this->assertEquals($tarjeta->obtenerSaldo(), 802,08);
+
+        $this->assertTrue($tarjeta->recargar(962,59));
+        $this->assertEquals($tarjeta->obtenerSaldo(), 1986,25);
+
+
     }
 
     /**
