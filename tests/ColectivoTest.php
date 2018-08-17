8a3e4bfb9a8da85ec171f6639ad87443a7064db4
MarianelaClara <marianelaclara@gmail.com>
2018-08-17 18:51:44 -0300

    TestSinSaldo

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -19,5 +19,27 @@ class ColectivoTest extends TestCase {
 		$tarjeta->recargar(10);
 		$boleto = new Boleto (14.80, $cole, (20-14.80));
 		$this->assertEquals($cole->pagarCon($tarjeta), $boleto);
-    }
+		}
+		
+    public function testSinSaldo(){
+			$cole= new Colectivo ("122 negro", 1, "Semtur");
+			$tarjeta= new Tarjeta;
+			$boleto = new Boleto ($tarjeta->valor, $cole, ($tarjeta->saldo - $tarjeta->valor));
+			$this->assertEquals($cole->pagarCon($tarjeta), $boleto);
+			$boleto1 = new Boleto ($tarjeta->valor, $cole, ($tarjeta->saldo - $tarjeta->valor));
+			$this->assertEquals($cole->pagarCon($tarjeta), $boleto1);
+		  $this->assertFalse($cole->pagarCon($tarjeta));
+		
+		}
+
+
+
+
+
+
+
+		}
+
+
+
 }
