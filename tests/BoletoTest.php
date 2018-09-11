e434938ebb13ea96756eaf4aa38c2d0126bc8068
MarianelaClara <41971422+MarianelaClara@users.noreply.github.com>
2018-09-11 17:41:53 -0300

    update test boleto

--- tests/BoletoTest.php
+++ tests/BoletoTest.php
@@ -11,4 +11,14 @@ class BoletoTest extends TestCase {
         $boleto = new Boleto("", $valor, 1, "Medio", 1, "Segundo Plus", "123 Negro", "Semtur", 1, date("d/m/Y H:i:s", 0));
         $this->assertEquals($boleto->obtenerValor(), $valor);
     }
+    public function testObtenerIdTarjeta(){
+        $valor = 14.80;
+        $boleto = new Boleto("", $valor, 3, "Medio", 1, "Segundo Plus", "123 Negro", "Semtur", 1, date("d/m/Y H:i:s", 0));
+        $this->assertEquals($boleto->obtenerIdTarjeta(), 3);
+    }
+    
+    public function testObtenerTipoTarjeta(){
+       $valor = 14.80;
+       $boleto = new Boleto("", $valor, 1, "Medio", 1, "Segundo Plus", "123 Negro", "Semtur", 1, date("d/m/Y H:i:s", 0));
+       $this->assertEquals($boleto->obtenerTipoTarjeta(),"Medio");
 }
