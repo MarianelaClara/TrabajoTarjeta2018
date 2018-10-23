6ca4ed6b556ccc85e1d3302ca539fe3be68b3622
Julio Bobe <juliounicornio891@gmail.com>
2018-10-23 10:44:23 -0300

    corrijo construct

--- src/TarjetaMedio.php
+++ src/TarjetaMedio.php
@@ -6,8 +6,9 @@ class TarjetaMedio extends Tarjeta {
     public $ultimoViaje= 0;
     protected $tiempoEspera= 60*5;
 
-    public function __construct() {
+    public function __construct($id = 1) {
         $this->valor = $this->valor /2;
+        $this->id = $id;
     }
 
     public function obtenerTiempoEspera(){
