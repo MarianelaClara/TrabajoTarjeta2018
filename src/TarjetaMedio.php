935c7b63566a581fe2839637d37c380456aff6f1
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2018-11-17 23:07:06 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/TarjetaMedio.php
+++ src/TarjetaMedio.php
@@ -3,15 +3,15 @@
 namespace TrabajoTarjeta;
 
 class TarjetaMedio extends Tarjeta {
-    public $ultimoViaje= 0;
-    protected $tiempoEspera= 60*5;
+    public $ultimoViaje = 0;
+    protected $tiempoEspera = 60 * 5;
 
     public function __construct($id = 1) {
-        $this->valor = $this->valor /2;
+        $this->valor = $this->valor / 2;
         $this->id = $id;
     }
 
-    public function obtenerTiempoEspera(){
+    public function obtenerTiempoEspera() {
         return $this->tiempoEspera;
     }
 
--- src/TarjetaMedioUni.php
+++ src/TarjetaMedioUni.php
@@ -3,29 +3,29 @@
 namespace TrabajoTarjeta;
 
 class TarjetaMedioUni extends Tarjeta {
-    protected $usoDeMedio= 0;
+    protected $usoDeMedio = 0;
     protected $diaDeUso;
  
-    public function __construct($dia=1) {
-        $this->valor = $this->valor /2;
-        $this->diaDeUso= $dia;
+    public function __construct($dia = 1) {
+        $this->valor = $this->valor / 2;
+        $this->diaDeUso = $dia;
     }
-    public function usarMedio(){
+    public function usarMedio() {
         $this->usoDeMedio++;    
     }
-    public function obtenerUsoDeMedio(){
+    public function obtenerUsoDeMedio() {
         return $this->usoDeMedio;
     }
  
-    public function actualizarDia($diaNuevo){
-        $this->diaDeUso= $diaNuevo;
-        $this->usoDeMedio=0;
+    public function actualizarDia($diaNuevo) {
+        $this->diaDeUso = $diaNuevo;
+        $this->usoDeMedio = 0;
     }
 
-    public function obtenerDiaDeUso(){
+    public function obtenerDiaDeUso() {
         return $this->diaDeUso;
     }
-    public function normalTransbordo(){
-        $this->saldo -= ($this->valor*2)*33/100;
+    public function normalTransbordo() {
+        $this->saldo -= ($this->valor * 2) * 33 / 100;
     }
 }
